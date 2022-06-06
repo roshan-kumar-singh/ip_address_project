bbd0a4579fe04dfe199e10b9f2dbcd4288af4755
roshan <roshankumarsingh9876@gmail.com>
2022-06-06 22:26:39 +0530

    adding button

--- src/App.js
+++ src/App.js
@@ -5,22 +5,28 @@ import axios from 'axios'
 function App() {
   const [ip, setIP] = useState('');
 
-  const getData = async () => {
-    const res = await axios.get('https://geolocation-db.com/json/')
-    console.log(res.data);
-    setIP(res.data.IPv4)
-  }
-  
-  useEffect( () => {
-    getData()
+const ipaddress=()=>{
+	const getData = async () => {
+		const res = await axios.get('https://geolocation-db.com/json/')
+		console.log(res.data);
+		setIP(res.data.IPv4)
+	  }
+	  getData()
+	
+	}
+//   useEffect( () => {
+    
 
-  }, [])
+//   }, [])
 
   return (
+	  
     <div className="App">
       <h2>My IP Address is</h2>
+	  {/* document.body.style.backgroundImage = "https://picsum.photos/200/300"; */}
+	  <button type="button" class="btn btn primary my-3" onClick={ipaddress}> check Ip Address</button>
       <h4>{ip}</h4>
-    </div>
+    </div>  
   );
 }
 
